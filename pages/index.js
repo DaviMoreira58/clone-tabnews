712b98b20dbc8caaad0bdff9d1f436c7939aa1c6
Davi Moreira <62727795+DaviMoreira58@users.noreply.github.com>
2024-01-20 22:57:07 +0000

    adiciona scripts `lint: check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
-function Home(){
-    return <h1>Larissa, amo você demais!!!</h1>
+function Home() {
+  return <h1>Larissa, amo você demais!!!</h1>;
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("teste 2");
 }
 
 export default Home;
